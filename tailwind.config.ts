278ff685999cdcb07d8fbf970925c7cfa760fc62
Paweł Chudecki <pawelc29@interia.pl>
2024-04-17 13:45:18 +0200

    basic working layout

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,13 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primaryRed: "hsl(0, 100%, 74%)",
+        primaryGreen: "hsl(154, 59%, 51%)",
+        accentBlue: "hsl(248, 32%, 49%)",
+        neutralDarkBlue: "hsl(249, 10%, 26%)",
+        neutralGrayishBlue: "hsl(246, 25%, 77%)",
+      },
     },
   },
   plugins: [],
