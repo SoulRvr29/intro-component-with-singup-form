7f0ddfe9e2d89170945a792a0722e2c4e5034b7a
Paweł Chudecki <pawelc29@interia.pl>
2024-04-18 09:57:56 +0200

    working validation and submit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,30 @@
 "use client";
 import { FaCircleExclamation } from "react-icons/fa6";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+};
 
 export default function Home() {
-  const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    alert("Form submitted");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    alert(`
+    Form submitted
+    ------------------
+    First Name: ${data.firstName} 
+    Last Name: ${data.lastName} 
+    Email: ${data.email} 
+    Password: ${data.password} 
+    `);
   };
   return (
     <main className="grid grid-cols-2 max-[900px]:grid-cols-1 max-[900px]:max-w-xl items-center max-w-6xl gap-8 p-6">
@@ -26,70 +46,116 @@ export default function Home() {
           </div>
         </div>
         <form
-          className="flex flex-col gap-4 bg-white rounded-lg p-10 max-[900px]:p-6 shadow-[0_8px_0_rgba(0,0,0,0.2)]"
-          onSubmit={(e) => submitHandler(e)}
+          className="flex flex-col gap-8 bg-white rounded-lg p-10 max-[900px]:p-6 shadow-[0_8px_0_rgba(0,0,0,0.2)]"
+          onSubmit={handleSubmit(onSubmit)}
         >
-          <div className="relative">
+          <div className="relative grid  items-center">
             <input
               className="w-full border rounded-md px-8 py-4 border-neutralGrayishBlue text-neutralDarkBlue placeholder:text-neutralDarkBlue placeholder:font-[500]"
               placeholder="First Name"
               type="text"
-              name="firstName"
               id="firstName"
+              {...register("firstName", { required: true, minLength: 3 })}
             />
-            <div className="absolute right-6 top-[calc(50%-13px)] hidden">
-              <FaCircleExclamation color="var(--primaryRed)" size={26} />
-            </div>
-            <p className="text-primaryRed text-xs text-right pt-1 h-4 hidden">
-              First Name cannot be empty
-            </p>
+            {errors.firstName && (
+              <div className="absolute right-6 ">
+                <FaCircleExclamation color="var(--primaryRed)" size={26} />
+              </div>
+            )}
+            {errors.firstName?.type === "required" && (
+              <p className="text-primaryRed text-xs text-right absolute right-0 -bottom-4">
+                First Name cannot be empty
+              </p>
+            )}
+            {errors.firstName?.type === "minLength" && (
+              <p className="text-primaryRed text-xs text-right absolute right-0 -bottom-4">
+                Minimum length is 3 characters
+              </p>
+            )}
           </div>
-          <div className="relative">
+          <div className="relative grid items-center">
             <input
               className="w-full border rounded-md px-8 py-4 border-neutralGrayishBlue text-neutralDarkBlue placeholder:text-neutralDarkBlue placeholder:font-[500]"
               placeholder="Last Name"
               type="text"
-              name="lastName"
               id="lastName"
+              {...register("lastName", { required: true, minLength: 3 })}
             />
-            <div className="absolute right-6 top-[calc(50%-13px)] hidden">
-              <FaCircleExclamation color="var(--primaryRed)" size={26} />
-            </div>
-            <p className="text-primaryRed text-xs text-right pt-1 h-4 hidden">
-              First Name cannot be empty
-            </p>
+            {errors.lastName && (
+              <div className="absolute right-6 ">
+                <FaCircleExclamation color="var(--primaryRed)" size={26} />
+              </div>
+            )}
+            {errors.lastName?.type === "required" && (
+              <p className="text-primaryRed text-xs text-right absolute right-0 -bottom-4">
+                Last Name cannot be empty
+              </p>
+            )}
+            {errors.lastName?.type === "minLength" && (
+              <p className="text-primaryRed text-xs text-right absolute right-0 -bottom-4">
+                Minimum length is 3 characters
+              </p>
+            )}
           </div>
-          <div className="relative">
+          <div className="relative grid items-center">
             <input
               className="w-full border rounded-md px-8 py-4 border-neutralGrayishBlue text-neutralDarkBlue placeholder:text-neutralDarkBlue placeholder:font-[500]"
               placeholder="Email Address"
               type="text"
-              name="email"
               id="email"
+              {...register("email", {
+                required: true,
+                pattern: /^[\w._]+@[\w]+\.[\w]{2,}$/i,
+              })}
             />
-            <div className="absolute right-6 top-[calc(50%-13px)] hidden">
-              <FaCircleExclamation color="var(--primaryRed)" size={26} />
-            </div>
-            <p className="text-primaryRed text-xs text-right pt-1 h-4 hidden">
-              First Name cannot be empty
-            </p>
+            {errors.email && (
+              <div className="absolute right-6 ">
+                <FaCircleExclamation color="var(--primaryRed)" size={26} />
+              </div>
+            )}
+            {errors.email && (
+              <p className="text-primaryRed text-xs text-right absolute right-0 -bottom-4">
+                Looks like this is not an email
+              </p>
+            )}
           </div>
-          <div className="relative">
+          <div className="relative grid items-center">
             <input
               className="w-full border rounded-md px-8 py-4 border-neutralGrayishBlue text-neutralDarkBlue placeholder:text-neutralDarkBlue placeholder:font-[500]"
               placeholder="Password"
-              type="text"
-              name="password"
+              type="password"
               id="password"
+              {...register("password", {
+                required: true,
+                minLength: 6,
+                pattern: /^(?=.*[\w])(?=.*\d).+$/,
+              })}
             />
-            <div className="absolute right-6 top-[calc(50%-13px)] hidden">
-              <FaCircleExclamation color="var(--primaryRed)" size={26} />
-            </div>
-            <p className="text-primaryRed text-xs text-right pt-1 h-4 hidden">
-              First Name cannot be empty
-            </p>
+            {errors.password && (
+              <div className="absolute right-6 ">
+                <FaCircleExclamation color="var(--primaryRed)" size={26} />
+              </div>
+            )}
+            {errors.password?.type === "required" && (
+              <p className="text-primaryRed text-xs text-right absolute right-0 -bottom-4">
+                First Name cannot be empty
+              </p>
+            )}
+            {errors.password?.type === "minLength" && (
+              <p className="text-primaryRed text-xs text-right absolute right-0 -bottom-4">
+                Minimum length is 6 characters
+              </p>
+            )}
+            {errors.password?.type === "pattern" && (
+              <p className="text-primaryRed text-xs text-right absolute right-0 -bottom-4">
+                Must be letters and numbers
+              </p>
+            )}
           </div>
-          <button className="text-center uppercase bg-primaryGreen rounded-md py-4 pb-3 font-[500] border-[rgb(37,170,113)]  border-b-4 hover:opacity-75">
+          <button
+            type="submit"
+            className="text-center uppercase bg-primaryGreen rounded-md py-4 pb-3 font-[500] border-[rgb(37,170,113)] -mb-6 border-b-4 hover:opacity-75"
+          >
             claim your free trial
           </button>
           <p className="text-neutralGrayishBlue text-center text-xs scale-[0.8] leading-6">
